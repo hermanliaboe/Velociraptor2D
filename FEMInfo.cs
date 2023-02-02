00e95d680cf7bf9e2426cace59a5dc182e25b558
hermanlliabo <hermalia@ntnu.no>
2023-02-02 16:47:55 +0100

    Add project files.

---
+++ FEMInfo.cs
@@ -0,0 +1,26 @@
+﻿using Grasshopper;
+using Grasshopper.Kernel;
+using System;
+using System.Drawing;
+
+namespace FEM
+{
+    public class FEMInfo : GH_AssemblyInfo
+    {
+        public override string Name => "FEM";
+
+        //Return a 24x24 pixel bitmap to represent this GHA library.
+        public override Bitmap Icon => null;
+
+        //Return a short string describing the purpose of this GHA library.
+        public override string Description => "";
+
+        public override Guid Id => new Guid("885d0b84-699d-4935-b0d2-158dc76c073b");
+
+        //Return a string identifying you or your company.
+        public override string AuthorName => "";
+
+        //Return a string representing your preferred contact details.
+        public override string AuthorContact => "";
+    }
+}
