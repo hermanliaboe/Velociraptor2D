d320f8be14ad02a81d6163bb6c880c2a3328315f
eliaskiaer <elias.kiaer@gmail.com>
2023-02-07 10:09:25 +0100

    Made CreateLoade 100+

---
+++ Classes/Load.cs
@@ -0,0 +1,24 @@
+﻿using Rhino.Geometry;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FEM.Classes
+{
+    internal class Load
+    {
+        public Point3d point;
+        public Vector3d vector;
+
+
+        public Load(){ }
+    
+        public Load(Point3d point, Vector3d vector) 
+        {
+            this.point = point;
+            this.vector = vector;
+        }
+    }
+}
