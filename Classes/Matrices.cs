e4b3c2d585dfea9af97ec53affa203134980b743
hermanlliabo <hermalia@ntnu.no>
2023-02-08 16:51:09 +0100

    updates on fem solver with matrices class

--- Classes/Matrices.cs
+++ Classes/Matrices.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,23 +10,76 @@ namespace FEM.Classes
 {
     internal class Matrices
     {
-        public LA.Matrix<Double> globalK;
-        public LA.Matrix<Double> globalM;
-        public LA.Matrix<Double> globalC;
-        public LA.Matrix<Double> globalN;
-        public LA.Matrix<Double> globalF;
+        public LA.Matrix<double> globalK;
+        public LA.Matrix<double> globalM;
+        public LA.Matrix<double> globalC;
+        public LA.Matrix<double> globalN;
+        public LA.Matrix<double> globalF;
+
+        public Matrices(int dof, List<BeamElement> elements, double E, double A, double I)
+        {
+            this.globalK = BuildGlobalK(dof, elements, E, A, I);
+
+        }
 
         public Matrices()
         {
 
         }
 
-        private LA.Matrix<Double> buildGlobalK(int dof, List<BeamElement> elements, double E, double A)
+        public LA.Matrix<double> BuildGlobalK(int dof, List<BeamElement> elements, double E, double A, double I)
         {
 
-            LA.Matrix<Double> K = LA.Matrix<Double>.Build.Dense(dof, dof);
+            LA.Matrix<double> globalK = LA.Matrix<double>.Build.Dense(dof, dof);
+            foreach (var element in elements)
+            {
+                int inod = element.startNode.globalID;
+                int jnod = element.endNode.globalID;
+                LA.Matrix<double> ke = GetKel(element, E, A, I);
+                
+            }
 
-            return K;
+            return globalK;
+        }
+
+        public LA.Matrix<double> GetKel(BeamElement beam, double E, double A, double I)
+        {
+            int nNode = 2;  //how many nodes per element
+
+
+            //gets length of element
+
+            Node startNode = beam.startNode;
+            double z1 = startNode.point.Z;
+            double x1 = startNode.point.X;
+
+            Node endNode = beam.endNode;
+            double z2 = startNode.point.Z;
+            double x2 = startNode.point.X;
+
+            double l = Math.Sqrt(Math.Pow(z2 - z1, 2) + Math.Pow(x2 - x1, 2));
+
+            LA.Matrix<double> kEl = LA.Matrix<double>.Build.Dense(nNode * 3, nNode * 3);
+
+            double ealA = (E * A) / l;
+            double ealB = 12 * (E * I) / Math.Pow(l, 3);
+            double ealC = 6 * (E * I) / Math.Pow(l, 2);
+            double ealD = 4 * (E * I) / l;
+            double ealE = 2 * (E * I) / l;
+
+            kEl[0, 1] = ealA; kEl[1, 1] = 0; kEl[2, 1] = 0; kEl[3, 1] = -ealA; kEl[4, 1] = 0; kEl[5, 1] = 0;
+            kEl[0, 2] = 0; kEl[1, 2] = ealB; kEl[2, 2] = -ealC; kEl[3, 2] = 0; kEl[4, 2] = -ealB; kEl[5, 2] = -ealC;
+            kEl[0, 3] = 0; kEl[1, 3] = -ealC; kEl[2, 3] = ealD; kEl[3, 3] = 0; kEl[4, 3] = ealC; kEl[5, 3] = ealE;
+            kEl[0, 4] = -ealA; kEl[1, 4] = 0; kEl[2, 4] = 0; kEl[3, 4] = ealA; kEl[4, 4] = 0; kEl[5, 4] = 0;
+            kEl[0, 5] = 0; kEl[1, 5] = -ealB; kEl[2, 5] = ealC; kEl[3, 5] = 0; kEl[4, 5] = ealB; kEl[5, 5] = ealC;
+            kEl[0, 6] = 0; kEl[1, 6] = -ealC; kEl[2, 6] = ealD; kEl[3, 6] = 0; kEl[4, 6] = ealC; kEl[5, 6] = ealE;
+
+            
+            LA.Matrix<double> T = LA.Matrix<double>.Build.Dense(nNode * 3, nNode * 3);
+
+
+
+            return kEl;
         }
     }
 }
--- Components/FEMSolver.cs
+++ Components/FEMSolver.cs
@@ -36,7 +36,7 @@ namespace FEM.Components
         /// </summary>
         protected override void RegisterInputParams(GH_InputParamManager pManager)
         {
-            //kode kode kode
+            pManager.AddGenericParameter("Assembly","ass","",GH_ParamAccess.item);
         }
 
         /// <summary>
@@ -44,6 +44,8 @@ namespace FEM.Components
         /// </summary>
         protected override void RegisterOutputParams(GH_OutputParamManager pManager)
         {
+            pManager.AddPointParameter("New points","","",GH_ParamAccess.list);
+            pManager.AddLineParameter("New geometry","lines","", GH_ParamAccess.list);
         }
 
         /// <summary>
@@ -53,81 +55,18 @@ namespace FEM.Components
         /// to store data in output parameters.</param>
         protected override void SolveInstance(IGH_DataAccess DA)
         {
-            // Get input and deconstruct assembly into beams, loads and supports. 
-            /*
-          List<Assembly> assembly = new List<Assembly>();
-          DA.GetData(0, ref assembly);
+            Classes.Assembly model = new Classes.Assembly();
+            int dof = model.nodeList.Count*3;
+            List<BeamElement> elements = new List<BeamElement>();
+            double E = 7000; //MPa
+            double A = 10000; //mm^2
 
-
-          beamList = assembly.beamList;
-          supportList = assembly.supportList;
-          loadList = assembly.loadList;
-
-          //Input for element definition:
-          double e = 1;   //E-moduls
-          double a = 0.1; //Areal
-          double v = 0.3; //Poisson-ratio, if neede
-
-          */
-
-            //End
-
-            //Assembly of global stiffness matrix
-
-
-
-
-
-
-            //End
-
-            //Applying BC to K
-            //End
-
-            //Calculate element stresses
-            //End
+            Matrices matrices = new Matrices();
+            LA.Matrix<Double> globalK = matrices.buildGlobalK(dof, elements, E, A);
+      
         }
 
-        LA.Matrix<double> getKel(BeamElement beam, double e, double a)
-        {
-            int nNode = 2;  //how many nodes per element
-
-
-            //gets length of element
-
-            Node startNode = beam.startNode;
-            double z1 = startNode.point.Z;
-            double x1 = startNode.point.X;
-
-            Node endNode = beam.endNode;
-            double z2 = startNode.point.Z;
-            double x2 = startNode.point.X;
-
-            double l = Math.Sqrt(Math.Pow(z2 - z1, 2) + Math.Pow(x2 - x1, 2));
-
-            LA.Matrix<double> kEl = LA.Matrix<double>.Build.Dense(nNode * 3, nNode * 3);
-
-            double ealA =    (e * a) / l;
-            double ealB = 12*(e * a) / Math.Pow(l,3);
-            double ealC = 6* (e * a) / Math.Pow(l, 2);
-            double ealD = 4* (e * a) / l;
-            double ealE = 2* (e * a) / l;
-
-            kEl[0, 1] = ealA;  kEl[1, 1] = 0;     kEl[2, 1] = 0;     kEl[3, 1] = -ealA; kEl[4, 1] = 0;     kEl[5, 1] = 0;
-            kEl[0, 2] = 0;     kEl[1, 2] = ealB;  kEl[2, 2] = -ealC; kEl[3, 2] = 0;    kEl[4, 2] = -ealB; kEl[5, 2] = -ealC;
-            kEl[0, 3] = 0;     kEl[1, 3] = -ealC; kEl[2, 3] = ealD;  kEl[3, 3] = 0;    kEl[4, 3] = ealC;  kEl[5, 3] = ealE;
-            kEl[0, 4] = -ealA; kEl[1, 4] = 0;     kEl[2, 4] = 0;     kEl[3, 4] = ealA; kEl[4, 4] = 0;     kEl[5, 4] = 0;
-            kEl[0, 5] = 0;     kEl[1, 5] = -ealB; kEl[2, 5] = ealC;  kEl[3, 5] = 0;    kEl[4, 5] = ealB;  kEl[5, 5] = ealC;
-            kEl[0, 6] = 0;     kEl[1, 6] = -ealC; kEl[2, 6] = ealD;  kEl[3, 6] = 0;    kEl[4, 6] = ealC;  kEl[5, 6] = ealE;
-
-
-
-
-
-
-
-            return kEl;
-        }
+        
 
         /// <summary>
         /// Provides an Icon for every component that will be visible in the User Interface.
